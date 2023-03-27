3e49770a7a278f5feff37effbc3dc52832e09c7e
Luca Sichi <luca.sichi@hotmail.com>
2023-03-28 00:37:30 +0200

    pca ply

---
+++ code/python/pca/readply.py
@@ -0,0 +1,47 @@
+import os
+
+
+def readPly(filename):
+    file = open(filename, 'r')
+    lines = file.readlines()
+    file.close
+    return lines
+
+def getVertList(lines):
+    verts = []
+    for line in lines:
+        if line.startswith('end_header'):
+            continue
+        elif line.startswith('element'):
+            continue
+        elif line.startswith('property'):
+            continue
+        elif line.startswith('format'):
+            continue
+        elif line.startswith('comment'):
+            continue
+        elif line.startswith('ply'):
+            continue
+        else:
+            coords = line.split(' ')
+            if len(coords) > 4:
+                verts.append(float(coords[0]))
+                verts.append(float(coords[1]))
+                verts.append(float(coords[2].replace('\n', '')))
+    return verts
+
+def getFilenames():
+    return os.listdir('plys')
+
+def getData():
+    data = []
+    for file in getFilenames():
+        data.append(getVertList(readPly('plys/' + file)))
+    return data
+
+d = getData()
+l = 0
+for i in d:
+    l = l + len(i)
+
+print(l)
